dcbf74c80b83fd53efc46e5ac373df010252f57d
Artem Tishenko <artem.200207@yandex.ru>
2024-02-18 16:51:59 +0400

    add themes, in local storage and context provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
-import React, { Suspense } from 'react'
+import React, { Suspense, useContext, useState } from 'react'
 import { Counter } from './components/Counter'
-import './index.scss'
+import './styles/index.scss'
 import { Route, Routes } from 'react-router-dom'
 import { Link } from 'react-router-dom'
 import { AboutPageAsync } from './components/pages/AboutPage/AboutPageAsync'
 import { MainPageAsync } from './components/pages/MainPage/MainPageAsync'
+import { useTheme } from './theme/useTheme'
+import { classNames } from './helpers/classNames/classNames'
 
 export const App = () => {
+
+  const {theme, toggleTheme} = useTheme();
+
   return (
-    <div className='app'>
+    <div className={classNames('app', {}, [ theme])}>
+      <button onClick={toggleTheme}>TOGGLE</button>
       <Link to={'/'}>Главная</Link>
       <Link to={'/about'}>О сайте</Link>
       <Suspense fallback={<div>Loading...</div>}>
