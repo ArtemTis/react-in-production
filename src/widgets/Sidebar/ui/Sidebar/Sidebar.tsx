a94bbe56259325b4510ae2f6e598664f51cabe66
Artem Tishenko <artem.200207@yandex.ru>
2024-02-25 17:46:16 +0400

    i18n translate plugin

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -2,6 +2,7 @@ import { classNames } from 'shared/lib/classNames/classNames'
 import cls from './Sidebar.module.scss'
 import { useState } from 'react';
 import { ThemeSwitcher } from 'widgets/ThemeSwitcher';
+import { LangSwitcher } from 'widgets/LangSwitcher';
 
 interface SidebarProps {
     className?: string;
@@ -20,6 +21,7 @@ export const Sidebar: React.FC<SidebarProps> = ({ className }) => {
             <button onClick={onToggle}>toggle</button>
             <div className={cls.switchers}>
                 <ThemeSwitcher />
+                <LangSwitcher className={cls.Lang} />
             </div>
         </div>
     )
